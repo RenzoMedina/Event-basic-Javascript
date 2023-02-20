2db4f3bc8492c66bdaf78b28fe30a11fae74278b
RenzoMedina <medinast30@gmail.com>
2023-02-19 21:48:42 -0300

    he finalizado con los evento de los 3 botones pendientes los cuales son modificar los azules elminar los rojos

--- main.js
+++ main.js
@@ -1,29 +1,71 @@
-const btnRojo= document.getElementById('btn1')
-const btnDelete= document.getElementById('btn5')
+//varibales para los botones
+const btnRojo = document.getElementById('btn1')
+const btnBlue = document.getElementById('btn2')
+const updateBlue = document.getElementById('btn3')
+const btnDeleteRed = document.getElementById('btn4')
+const btnDelete = document.getElementById('btn5')
 
-const cardBox= document.getElementById('card-box')
+const cardBox = document.getElementById('card-box')
 
 //img
 let rojo = "Img/rojo.jpeg"
-let azul = "Img/azul.jpeg"
+let azul = "Img/azul.jpg"
 
+//event mouse to the buttons
 btnRojo.addEventListener('click', ()=>{
     addRojo()
 })
 btnDelete.addEventListener('click', ()=>{
     deleteBox()
 })
-
+btnBlue.addEventListener('click', ()=>{
+    addBlue()
+})
+btnDeleteRed.addEventListener('click', ()=>{
+    deleteRed()
+})
+updateBlue.addEventListener('click', ()=>{upBlues()})
 
 //function addRojo
 function addRojo(){
-    let item = document.createElement('img')
-    item.setAttribute('src', `${rojo}`)
-    item.classList.add('box')
+
+    let item = setAttributes('img','src',rojo,'rojo')
     cardBox.appendChild(item)
 }
-
 //function deleteBox
 function deleteBox(){
     cardBox.innerHTML=" "
+}
+//function addBlue four object
+function addBlue(){
+    
+    let item = `<img src="${azul}" class="azul">`
+    for(let i =0; i<4; i++){        
+        cardBox.insertAdjacentHTML('beforeend',item)
+    }
+}
+//function setAttribute
+function setAttributes(tipo, attribute,link, clase){
+
+    let item = document.createElement(`${tipo}`)
+    item.setAttribute(`${attribute}`,`${link}`)
+    item.classList.add(`${clase}`)
+
+    return item
+}
+//function detele red img
+function deleteRed(){
+    let count = document.querySelectorAll(".rojo")
+    for(let i =0; i < count.length;i++){
+        count[i].remove()
+    }
+}
+//function upBlues
+function upBlues(){
+    let count = document.querySelectorAll(".azul")
+    for (let i =0; i < count.length; i++){
+        count[i].src=rojo
+        count[i].classList.remove('azul')
+        count[i].classList.add('rojo')
+    }
 }
